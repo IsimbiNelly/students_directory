d3dc4f1c1ce8337afbd11391f9d63902ea7f93d2
innocent-gift <i.nkurunziz@alustudent.com>
2025-01-29 14:48:05 +0200

    I have updated

--- the_coder.sh
+++ the_coder.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
 mkdir students_directory
 touch students_directory/cohort-1.txt
 touch students_directory/cohort-2.txt
 touch students_directory/cohort-3.txt
-echo "oba" >> cohort-1.txt
-echo "eloi" >> cohort-1.txt
-echo "nelly" >> cohort-1.txt
+echo "oba" >> students_directory/cohort-1.txt
+echo "eloi" >> students_directory/cohort-1.txt
+echo "nelly" >> students_directory/cohort-1.txt
+echo "innocent" >> students_directory/cohort-2.txt
+echo "nelly" >> students_directory/cohort-2.txt
+echo "eloi" >>students_directory/cohort-2.txt
