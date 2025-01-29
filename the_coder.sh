fa219d1e646d39e8336bc018d9e0580f7d14ae6f
Eloi Iradukunda <e.iradukund2@alustudent.com>
2025-01-29 12:38:40 +0200

    Uploaded the Coder Shell Script

---
+++ the_coder.sh
@@ -0,0 +1,7 @@
+mkdir students_directory
+touch students_directory/cohort-1.txt
+touch students_directory/cohort-2.txt
+touch students_directory/cohort-3.txt
+echo "oba" >> cohort-1.txt
+echo "eloi" >> cohort-1.txt
+echo "nelly" >> cohort-1.txt
